a76dcd5c326be4c84296c6cf65399b7abc002570
Ghostemaner <DronByLive@mail.ru>
2021-07-23 15:32:26 +0300

    created form for messages and privitive bot

--- src/App.js
+++ src/App.js
@@ -1,15 +1,62 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Message from './Message';
 
 const text = "const's value";
 
+const authors = {
+  person: 'Guest',
+  bot: "BOT"
+}
+
 function App() {
+  const [messageList, setMessageList] = React.useState([])
+
+  const [textValue, setTextValue] = React.useState('')
+
+  
+  const handleMessageChange = (e) => {
+    setTextValue(e.target.value)
+  }
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault()
+    setMessageList((currentMessageList) => 
+    [ ...currentMessageList, 
+    {
+      author: authors.person, 
+      text: textValue
+    },
+    ])
+    setTextValue("")
+  }
+
+  React.useEffect(() => {
+    if (messageList.length && messageList[messageList.length - 1].author !== authors.bot) {
+      setTimeout(() => {
+        setMessageList((currentMessageList) => 
+        [ ...currentMessageList, 
+        {
+          author: authors.bot, 
+          text: "Вас приветствует бот!"
+        },
+        ])
+        }, 1500)
+    }
+    
+  }, [messageList])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Message text={text}/>
-        </header>
+    <div className="App App-header">
+      <div className="messageList">
+        {messageList.map((message, index) => <React.Fragment key={index}><div className="message">{message.author}: {message.text}</div></React.Fragment>)}
+      </div>
+      <form className="form" onSubmit={handleMessageSubmit}>
+        <input className="input" required placeholder="Введите сообщение" value={textValue} onChange={handleMessageChange}/>
+        <button className="button">send message</button>
+      </form>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function Message (props) {
-    return <p className='text'>Text from props: <span className="message">{props.text}</span> </p>
-};
+    
+    return (
+        <>
+            <p className='text'>Text from props: <span className="message">{props.text}</span> </p>
+        </>
+    
+    )};
 
 export default Message;
